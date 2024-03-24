7566ee5eb89665c57f4ed2a7093a35c320fcbd08
saxtonvandalsen <saxtonvandalsen@gmail.com>
2024-03-24 03:11:41 +0000

    testing removing manifest file

--- obj/Debug/net8.0/ms-cloud-ai-challenge.AssemblyInfo.cs
+++ obj/Debug/net8.0/ms-cloud-ai-challenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ms-cloud-ai-challenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+452e223c70af8033f124d7e4be1b7e51e87b6842")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+792ae08e78d5a0ff9b990bc9fa0219c1f73537bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ms-cloud-ai-challenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ms-cloud-ai-challenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
